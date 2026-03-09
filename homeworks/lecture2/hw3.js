a3371eee871c86c90f8229fc3acb033b13ef4ef2
yukun-tech <yukunwpi@gmail.com>
2026-03-08 18:51:45 -0700

    Update hw3.js

--- homeworks/lecture2/hw3.js
+++ homeworks/lecture2/hw3.js
@@ -2,29 +2,40 @@
 // Explain your answer.
 
 console.log(0.1 + 0.2);
+0.3
 
 console.log(0.1 + 0.2 == 0.3);
+true
 
 console.log(1 +  "2" + "2");
+122
 
 console.log(1 +  +"2" + "2");
+1 22
 
 console.log(1 +  -"1" + "2");
+Error
 
 console.log(+"1" +  "1" + "2");
 
+112
 console.log( "A" - "B" + "2");
+Error
 
 console.log( "A" - "B" + 2);
-
+Error
 console.log("0 || 1 = "+(0 || 1));
+0 || 1 = true
 
 console.log("1 || 2 = "+(1 || 2));
-
+1 || 2 = true
 console.log("0 && 1 = "+(0 && 1));
+0 && 1 = false
 
-console.log("1 && 2 = "+(1 && 2));
+console.log("(0 && 1)"+(1 && 2));
+(0 && 1)true
 
 console.log(false == '0')
-
+true
 console.log(false === '0')
+false
