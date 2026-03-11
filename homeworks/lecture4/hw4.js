509ac06c883096e581aeebaf34a22103235dcb9b
qzsgmhahaha <yanglin1999624@163.com>
2026-03-10 19:47:20 -0500

    Complete lecture4-hw

--- homeworks/lecture4/hw4.js
+++ homeworks/lecture4/hw4.js
@@ -12,4 +12,20 @@
  */
 const intersection = (nums1, nums2) => {
   // Your solution here
+  let result = []
+  // nums2 first because the case 2 Output: [9, 4]
+  for(let i = 0; i < nums2.length; i++) {
+    for(let j = 0; j < nums1.length; j++) {
+      if (nums2[i] === nums1[j]){
+        if (!result.includes(nums2[i])) {
+          result.push(nums2[i]);
+        }
+      }
+    }
+  }
+
+  return result;
 };
+
+console.log(intersection([1,2,2,1], [2,2])); // [2]
+console.log(intersection([4,9,5], [9,4,9,8,4])); // [9,4]
