6c9769ded41ceb0f0476b7052c2dc78ba59c5f6d
yukun-tech <yukunwpi@gmail.com>
2026-03-16 21:49:05 -0700

    Update hw1.js

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,28 @@
  */
 
 // your code here
+//引入核心模块
+const fs = require('fs');
+const path = require('path');
+/*
+process.argv[0] Node.js 程序的绝对路径。也就是“是谁在运行这个脚本” /usr/local/bin/node
+process.argv[1] 当前执行的 JS 文件的绝对路径。也就是“运行的是哪个文件” /Users/yourname/hw1.js
+process.argv[2] 用户传入的第一个真正参数./myFolder
+process.argv[3] 用户传入的第二个参数txt
+*/
+const folder = process.argv[2]; // 文件夹路径
+const ext = process.argv[3];    // 要过滤的后缀（例如 'txt'）
+fs.readdir(folder, (err, files) => { //readdir是目录
+  if (err) {
+    return console.error(err);
+  }
+  // 开始循环处理每一个文件
+  files.forEach(file => {
+    // path.extname(file) 会得到类似 '.txt' 或 '.js' 的字符串
+    // 我们把它跟 '.' + ext 拼接后的结果进行对比
+    if (path.extname(file) === '.' + ext) {
+      console.log(file); // 符合条件，打印文件名
+    }
+  });
+  
+});
