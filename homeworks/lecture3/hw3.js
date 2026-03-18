215a250ff6fd383e8487d1728f31f01ec16bcae1
jz450 <jieying.zhang@duke.edu>
2026-03-18 21:32:06 +0800

    finish l3-hw3

--- homeworks/lecture3/hw3.js
+++ homeworks/lecture3/hw3.js
@@ -1,8 +1,17 @@
 function counter() {
     // implement here
+    let ans = 0;
+    
+    return function(a) {
+        if (a !== undefined) {
+            ans += a;
+        }
+        return ans;
+    }
 }
 
 let count = counter();
 console.log(count(3));  // Output: 3
 console.log(count(5));  // Output: 8 (3 + 5)
 console.log(count());   // Output: 8
+
