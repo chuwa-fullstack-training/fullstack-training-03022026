1a4784bc594c95d51fb452c0ca3292b5217d6ffc
jz450 <jieying.zhang@duke.edu>
2026-03-18 19:43:49 +0800

    finish hw1

--- homeworks/lecture2/hw1.js
+++ homeworks/lecture2/hw1.js
@@ -5,8 +5,16 @@
 */
 function extend(o, p) {
     // implement your code here
-    test
+    for (let property in p) {
+        o[property] = p[property];
+    }
+    return o;
 }
+
+// test 
+// let o = {a: 1};
+// let p = {a: 2, id: 3, name: 'alice'};
+// console.log(extend(o, p));
 
 /*
 * Return a new object that holds the properties of both o and p.
@@ -14,7 +22,20 @@ function extend(o, p) {
 */
 function union(o, p) {
     // implement your code here
+    let op = {};
+    for (let property in p) {
+        op[property] = p[property];
+    }
+    for (let property in o) {
+        op[property] = o[property];
+    }
+    return op;
 }
+
+// test
+// let o = {id: 1, name: 'abc'};
+// let p = {id: 2, age: 3};
+// console.log(union(o, p));
 
 /*
 * Remove properties from o if there is not a property with the same name in p.
@@ -22,7 +43,18 @@ function union(o, p) {
 */
 function restrict(o, p) {
     // implement your code here
+    for (let property in o) {
+       if (!(property in p)) {
+            delete o[property];
+       }
+    }
+    return o;
 }
+
+// test
+// let o = {a: 1, b: 200, c: 3};
+// let p = {a: 3, b: 2};
+// console.log(restrict(o, p));
 
 /*
 * Return a new object that holds only the properties of o that also appear
@@ -31,4 +63,16 @@ function restrict(o, p) {
 */
 function intersection(o, p) {
     // implement your code here
+    let op = {};
+    for (let property in o) {
+        if (property in p) {
+            op[property] = o[property];
+        }
+    }
+    return op;
 }
+
+// test
+// let o = {a: 1, b: 2, c: 'alice'};
+// let p = {a: 2, c: 'zh'};
+// console.log(intersection(o, p));
