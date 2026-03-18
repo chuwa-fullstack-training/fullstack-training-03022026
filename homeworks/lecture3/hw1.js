95124226b75fc03fb058abc75d8032458f1df89f
jz450 <jieying.zhang@duke.edu>
2026-03-18 21:11:22 +0800

    finish l3-hw1

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -5,4 +5,22 @@
  */
 function pickCoins() {
     // implement here
+    let cnt = 0;
+    for (let d = 0; d <= 2; ++d) {
+        for (let c = 0; c <= 4; ++c) {
+            for (let b = 0; b <= 20; ++b) {
+                let a = 48 - d - b - c;
+                if (a < 0) {
+                    continue;
+                }
+                if (a + 5 * b + 25 * c + 50 * d === 100) {
+                    console.log(a, b, c, d);
+                    cnt++;
+                }
+                if (cnt === 2) {
+                    return;
+                }
+            }
+        }
+    }
 }
