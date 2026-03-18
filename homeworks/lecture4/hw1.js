d80e39b651866739ba899dd4d0ead08e4318e000
jz450 <jieying.zhang@duke.edu>
2026-03-19 00:17:14 +0800

    finish l4-hw1

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -7,4 +7,38 @@
 
 function checkValidHTML(html) {
     // implement your solution here
+    let stack = [];
+    let i = 0;
+
+    while (i < html.length) {
+        if (html[i] === '<') {
+            let j = i;
+            while (j < html.length && html[j] !== '>') {
+                j++;
+            }
+            let tag = html.slice(i + 1, j);
+
+            if (tag[0] === '/') {
+                let name = tag.slice(1);
+                if (stack.length === 0) {
+                    return false;
+                }
+                if (stack.pop() !== name) {
+                    return false;
+                }
+            }
+            else {
+                stack.push(tag);
+            }
+            i = j + 1;
+        }
+        else {
+            i++;
+        }
+    }
+    return stack.length === 0;
 }
+
+console.log(checkValidHTML('<html><head><title>My Title</title></head></html>'));
+console.log(checkValidHTML('<html><head><title>My Title</title></head></head></html>'));
+console.log(checkValidHTML('<html><head><title>My Title</title></head></html'));
