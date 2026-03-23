402e673c1a95efa85c8111c6f87f56eb5d4294f7
jz450 <jieying.zhang@duke.edu>
2026-03-23 21:55:29 +0800

    finish l5-hw2

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -7,9 +7,27 @@
  * extra: in-place
  * @param {string[]} str
  */
+function reverseL(arr, left, right) {
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+}
 function reverseWords(str) {
   // your code here
+  reverseL(str, 0, str.length - 1);
+  let i = 0;
+  while (i < str.length) {
+    let l = i;
+    while (i < str.length && str[i] !== ' ') {
+      i++;
+    }
+    reverseL(str, l, i - 1);
+    i++;
+  }
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
 reverseWords(input);
+console.log(input)
