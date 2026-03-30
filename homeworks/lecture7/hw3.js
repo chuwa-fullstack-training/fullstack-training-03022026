fa43e8a30288c2c8183fc3dfa36b5e5dc67fb4fe
Freya379 <125752050+Freya379@users.noreply.github.com>
2026-03-30 12:21:53 -0700

    same

--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -9,3 +9,66 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (method === 'GET') {
+    if (url === '/') {
+      res.end('this is the home page');
+    } else if (url === '/about') {
+      res.end('this is the about page');
+      //2: read home.html's query string and display it on the page
+    } else if (url.startsWith('/home.html')) {
+      fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
+        if (err) {
+          res.end('error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(html);
+          // to see if we have query string or not
+          const urlObj = new URL(url, 'http://localhost:3000');
+          const title = urlObj.searchParams.get('title');
+          const content = urlObj.searchParams.get('content');
+
+          if (title || content){
+            res.write(`<h2>Submitted Data:<h2>`);
+            res.write(`<p>Title: ${title}</p>`);
+            res.write(`<p>Content: ${content}</p>`);
+          }
+          res.end();
+        }
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else if (method === 'POST') {
+    if (url === '/create-post') {
+      let body = [];
+      req.on('data', chunk => {
+        body.push(chunk);
+      });
+      // 1: redirect to home.html with query string
+      req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        res.writeHead(302, {'Location': '/home.html?' + parsedBody});
+        res.end();
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else {
+    res.end('Unsupported method');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+// 302 是 HTTP 状态码，意思是"你要的东西搬家了，去这个新地址"。浏览器收到 302 会自动跳转到 Location 指定的地址。
